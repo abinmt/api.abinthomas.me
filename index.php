cc2aa6b28100c6fb298ece31b8b5678aa3a76916
Abin Mathew Thomas <abinmt@gmail.com>
2018-08-20 08:43:51 +0530

    mongo db connection

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+require 'vendor/autoload.php';
+
+$client = new MongoDB\Client("mongodb://localhost:27017");
+$collection = $client->blog_abinthomas_me->posts;
+
+$result = $collection->find();
+
+foreach ($result as $entry) {
+    echo $entry['_id'], ': ', $entry['name'], "\n";
+}
+
+exit();
+
+/*
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
@@ -6,10 +21,20 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+
+    $connection = new MongoClient();
+    $db = $connection->blog_abinthomas_me;
+
+    $posts = $db->posts;
+
+     // all records
+    $result = $posts->find();
+
     $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+    $response->getBody()->write("Hello, $result");
 
     return $response;
 });
 
 $app->run();
+*/
