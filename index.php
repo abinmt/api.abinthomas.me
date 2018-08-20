45b5eac689532680e60470ef7b217e1dfab44176
Abin Mathew Thomas <abinmt@gmail.com>
2018-08-20 09:16:30 +0530

    ..

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ require 'vendor/autoload.php';
 $client = new MongoDB\Client("mongodb://localhost:27017");
 $collection = $client->blog_abinthomas_me->posts;
 
-$result = $collection->find();
+$result = $collection->find()->sort(['created_on']);
+
 
 foreach ($result as $entry) {
     echo $entry['_id'], ': ', $entry['name'], "\n";
@@ -13,6 +14,23 @@ foreach ($result as $entry) {
 exit();
 
 /*
+https://docs.mongodb.com/php-library/current/tutorial/
+_id
+
+name
+
+status
+
+created_on
+
+updated_on
+
+contents
+
+tags
+
+comments
+
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
